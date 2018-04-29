ab19b7868580fde0b6c295b7701a2c76f15a3436
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-29 00:14:31 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,6 +9,7 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    project.add_backer(self)
+    project.add_backer(self) if !project.backers.include?(self.name)
+
   end
 end
